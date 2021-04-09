0616b104f904c7d8c64882b29c68e0dd20c3c4e3
Trung DG <thanhtrung.it25@gmail.com>
2021-04-09 11:23:25 +0700

    unit test and travis config

---
+++ test.js
@@ -0,0 +1,9 @@
+const test = require("tape");
+test("test integer addition", (t) => {
+  t.plan(1);
+  t.equal(1 + 1, 2);
+});
+test("test string addition", (t) => {
+  t.plan(1);
+  t.equal("1" + "1", 2);
+});
